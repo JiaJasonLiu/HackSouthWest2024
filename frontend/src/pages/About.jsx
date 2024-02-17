7c8ec971afcfb5a0fd004da352abffd60e6368c1
threelyK <scmichael23@yahoo.com>
2024-02-17 20:07:19 +0000

    Changed About.jsx, About.css and added image

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,24 +1,29 @@
-import '../index.css';
-import '../about.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import AboutBoxMain from '../components/aboutboxmain';
-import Review from '../components/review';
+import '../css/index.css';
+import '../css/about.css';
 
 function About() {
 
   return (
     <div className="About">
-          <AboutBoxMain></AboutBoxMain>
-          <Review style = {{ margin: '50px'}}></Review>
           <div style = {{ marginTop: '50px'}}>
             <h2>The Asians++ Team</h2>
-            <p>Team Members: Yousuf Ahmed, Will Baldwin, Kyle Deladia, Jason Liu</p>
+            <h4>Team Members</h4>
+            <div className='memberInfoLeft'>
+              <p id='YA'>
+                <h6 className='nameLeft'>Yousuf Ahmed</h6>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores ullam dolorum fuga fugiat doloribus, magni non. At consequatur, molestias nihil magni perspiciatis ipsum asperiores alias natus delectus. Autem, itaque distinctio!
+              </p>
+            </div>
+            <p id='WB'>Will Baldwin</p>
+            <p id='KD'>Kyle Deladia</p>
+            <p id='JL'>Jason Liu</p>
+            <p id='DT'>Dariush Tomlinson</p>
             <ul>
-              <a href='https://github.com/yousufaahmed'>Yousuf Ahmed</a>
-              <a href='https://github.com/William-Baldwin'>Will Baldwin</a>
-              <a href='https://github.com/threelyK'>Kyle Deladia</a>
-              <a href=''>Dariush Tomlinson</a>
-              <a href='https://github.com/JiaJasonLiu'>Jason Liu</a>
+                <li><a href='https://github.com/yousufaahmed'>Yousuf Ahmed</a></li>
+                <li><a href='https://github.com/William-Baldwin'>Will Baldwin</a></li>
+                <li><a href='https://github.com/threelyK'>Kyle Deladia</a></li>
+                <li><a href=''>Dariush Tomlinson</a></li>
+                <li><a href='https://github.com/JiaJasonLiu'>Jason Liu</a></li>
             </ul>
           </div>
     </div>
