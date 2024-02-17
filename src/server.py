383785439443277e45a73661371bd004285895f5
William Baldwin <williamthebud@live.co.uk>
2024-02-17 15:39:30 +0000

    added request import

--- src/server.py
+++ src/server.py
@@ -1,6 +1,6 @@
 """The main webserver for the project"""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder="../frontend", static_folder="../frontend")
 
