13cfa0cafdfecfb4cba59fabce3581d6505483d3
threelyK <scmichael23@yahoo.com>
2024-02-17 18:35:23 +0000

    Update About.jsx

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,15 +1,25 @@
-// import '../index.css';
-// import '../about.css';
-// import AboutBoxMain from '../components/aboutboxmain';
-// import Review from '../components/review';
+import '../index.css';
+import '../about.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AboutBoxMain from '../components/aboutboxmain';
+import Review from '../components/review';
 
 function About() {
+
   return (
     <div className="About">
-          {/* <AboutBoxMain></AboutBoxMain> */}
-          {/* <Review style = {{ margin: '50px'}}></Review> */}
+          <AboutBoxMain></AboutBoxMain>
+          <Review style = {{ margin: '50px'}}></Review>
           <div style = {{ marginTop: '50px'}}>
-            <p>HELLO WORLD!</p>
+            <h2>The Asians++ Team</h2>
+            <p>Team Members: Yousuf Ahmed, Will Baldwin, Kyle Deladia, Jason Liu</p>
+            <ul>
+              <a href='https://github.com/yousufaahmed'>Yousuf Ahmed</a>
+              <a href='https://github.com/William-Baldwin'>Will Baldwin</a>
+              <a href='https://github.com/threelyK'>Kyle Deladia</a>
+              <a href=''>Dariush Tomlinson</a>
+              <a href='https://github.com/JiaJasonLiu'>Jason Liu</a>
+            </ul>
           </div>
     </div>
   );
