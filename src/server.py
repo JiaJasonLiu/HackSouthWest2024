85062983693dd383c2ac08dc26fefe9872dcb2aa
William Baldwin <williamthebud@live.co.uk>
2024-02-17 16:33:29 +0000

    Added first API call

--- src/server.py
+++ src/server.py
@@ -1,14 +1,23 @@
 """The main webserver for the project"""
 
+import server_funcs
 from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__, template_folder="../frontend", static_folder="../frontend")
 
 @app.route("/")
 def send_frontend():
-    """Serves the frontend to the client"""
+    """Serves the frontend to the client - might need replacing during integration"""
 
     return render_template("index.html")
+
+@app.route("/get_stock_price")
+def get_stock_price():
+    """Handles the get_stock_price API call"""
+    response_data = server_funcs.get_stock_info()
+
+    return json.dumps(response_data)
 
 if __name__ == ("__main__"):
     app.run()
---
+++ src/server_funcs.py
@@ -0,0 +1,6 @@
+"""Contains functions for the server to run"""
+
+def get_stock_info():
+    """Returns stock info dict to be sent to server"""
+
+    return 0
