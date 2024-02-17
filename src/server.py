3fa3f1cca40de070f912137985a6dd9046d9ee6d
William Baldwin <williamthebud@live.co.uk>
2024-02-17 15:37:26 +0000

    re-worked server to flask, added test statics

--- src/server.py
+++ src/server.py
@@ -1,33 +1,14 @@
 """The main webserver for the project"""
 
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from flask import Flask, render_template
 
-hostName = "localhost"
-serverPort = 8080
+app = Flask(__name__, template_folder="../frontend", static_folder="../frontend")
 
-class MainServer(BaseHTTPRequestHandler):
-    """The main webserver class for the project"""
+@app.route("/")
+def send_frontend():
+    """Serves the frontend to the client"""
 
-    def do_GET(self):
-        """The main get response for the project"""
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>",
-                                "utf-8"))
-        self.wfile.write(bytes(f"<p>Request: {self.path}</p>", "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+    return render_template("index.html")
 
-if __name__ == "__main__":
-    webServer = HTTPServer((hostName, serverPort), MainServer)
-    print(f"Server started http://{hostName}:{serverPort}")
-
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-
-    webServer.server_close()
-    print("Server stopped.")
+if __name__ == ("__main__"):
+    app.run()
