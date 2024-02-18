4bbd9f084854b9704073925e046152bd05a11d42
threelyK <scmichael23@yahoo.com>
2024-02-18 10:02:18 +0000

    Update aboutBox.jsx

--- frontend/src/components/aboutBox.jsx
+++ frontend/src/components/aboutBox.jsx
@@ -2,12 +2,11 @@ import '../css/index.css';
 import '../css/home.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ContentCard from '../components/contentCard';
-import UoE_logo from '../images/UoE_logo.png';
-import UoB_logo from '../images/UoB_logo.gif';
-
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+
+import UoE_logo from '../images/UoE_logo.png';
+import UoB_logo from '../images/UoB_logo.gif';
 
 let imgList = [
   'https://avatars.githubusercontent.com/u/76410737?v=4',
@@ -18,11 +17,11 @@ let imgList = [
 ]
 let memberList = ['Yousuf Ahmed', 'Will Baldwin', 'Kyle Deladia', 'Jason Liu', 'Dariush Tomlinson']
 let textList = [
-  'Lorem ipsum',
-  'Lorem ipsum',
-  'Lorem ipsum',
-  'Lorem ipsum',
-  'Lorem ipsum'
+  'University of Exeter',
+  'University of Exeter',
+  'University of Exeter',
+  'University of Bath',
+  'University of Exeter'
 ]
 let gitLink = [
   'https://github.com/yousufaahmed',
@@ -55,59 +54,6 @@ function AboutBox(){
                   
                 </Row>
               </div>
-
-
-
-            {/*<div className='memberInfo contentLeft'>
-              <p>
-                <h6 className='nameLeft'>
-                <a href='https://github.com/yousufaahmed' target='_blank' rel="noreferrer noopener"><img src='https://img.icons8.com/?size=256&id=62856&format=png' width='20px' height='20px'/>
-                Yousuf Ahmed
-                </a>
-                </h6>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores ullam dolorum fuga fugiat doloribus, magni non. At consequatur, molestias nihil magni perspiciatis ipsum asperiores alias natus delectus. Autem, itaque distinctio!
-              </p>
-            </div>
-            <div>
-              <p className='memberInfo contentRight'>
-                <h6 className='nameRight'>
-                <a href='https://github.com/William-Baldwin' target='_blank' rel="noreferrer noopener"><img src='https://img.icons8.com/?size=256&id=62856&format=png' width='20px' height='20px'/>
-                Will Baldwin
-                </a>
-                </h6>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus mollitia, officiis nemo sequi fugit dolor eius ipsa? Ut voluptates quas, accusantium tempore tempora nihil enim inventore delectus! Repudiandae, qui neque.
-              </p>
-            </div>
-            <div className='memberInfo contentLeft'>
-              <p>
-                <h6 className='nameLeft'>
-                <a href='https://github.com/threelyK' target='_blank' rel="noreferrer noopener"><img src='https://img.icons8.com/?size=256&id=62856&format=png' width='20px' height='20px'/>
-                Kyle Deladia
-                </a>
-                </h6>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam sed consequuntur officiis voluptates eveniet debitis, quis aut aspernatur in fuga molestias consectetur voluptatum obcaecati minus eos perspiciatis fugit autem alias.
-              </p>
-            </div>
-            <div>
-              <p className='memberInfo contentRight'>
-                <h6 className='nameRight'>
-                <a className='nameRight' href='https://github.com/JiaJasonLiu' target='_blank' rel="noreferrer noopener"><img src='https://img.icons8.com/?size=256&id=62856&format=png' width='20px' height='20px'/>
-                Jason Liu
-                </a>
-                </h6>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus illum repellat animi maiores? Autem asperiores ducimus veniam, corporis soluta dolor illum, repudiandae doloremque velit inventore nisi? Labore veniam cupiditate dicta.
-              </p>
-            </div>
-            <div className='memberInfo contentLeft'>
-              <p>
-                <h6 className='nameLeft'>
-                <a href='https://github.com/notdari' target='_blank' rel="noreferrer noopener"><img src='https://img.icons8.com/?size=256&id=62856&format=png' width='20px' height='20px'/>
-                Dariush Tomlinson
-                </a>
-                </h6>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum beatae minima consectetur, repudiandae, vitae assumenda ab minus accusantium quidem commodi rem, ducimus provident aperiam numquam blanditiis! Illo facilis neque quas.
-              </p>
-            </div> */}
 
             <p className='Disclaimer'>
               <b>Disclaimer:</b> This website is an educational tool to teach individuals about financial scams.
