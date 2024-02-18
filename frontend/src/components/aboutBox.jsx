ff874e89d50be1f7376ac62750a40d63197da57f
threelyK <scmichael23@yahoo.com>
2024-02-18 09:56:44 +0000

    ContentCard now working

--- frontend/src/components/aboutBox.jsx
+++ frontend/src/components/aboutBox.jsx
@@ -5,6 +5,33 @@ import ContentCard from '../components/contentCard';
 import UoE_logo from '../images/UoE_logo.png';
 import UoB_logo from '../images/UoB_logo.gif';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+let imgList = [
+  'https://avatars.githubusercontent.com/u/76410737?v=4',
+  'https://avatars.githubusercontent.com/u/45295662?v=4',
+  'https://avatars.githubusercontent.com/u/146539806?v=4',
+  'https://avatars.githubusercontent.com/u/95042312?v=4',
+  'https://avatars.githubusercontent.com/u/87239935?v=4'
+]
+let memberList = ['Yousuf Ahmed', 'Will Baldwin', 'Kyle Deladia', 'Jason Liu', 'Dariush Tomlinson']
+let textList = [
+  'Lorem ipsum',
+  'Lorem ipsum',
+  'Lorem ipsum',
+  'Lorem ipsum',
+  'Lorem ipsum'
+]
+let gitLink = [
+  'https://github.com/yousufaahmed',
+  'https://github.com/William-Baldwin',
+  'https://github.com/threelyK',
+  'https://github.com/JiaJasonLiu',
+  'https://github.com/notdari'
+]
+
 function AboutBox(){
     return(
         <div className =  'centered' style={{ color: '#000000'}}>
@@ -18,9 +45,15 @@ function AboutBox(){
                 <img src={UoB_logo} alt='University of Bath Logo' width='200px' height='82px' />
               </div>
 
-
-              <div className=''>
-                <ContentCard></ContentCard>
+              <div className='memberInfoCard'>
+                <Row>
+                  <Col><ContentCard profileImage={imgList[0]} member={memberList[0]} text={textList[0]} link={gitLink[0]}></ContentCard></Col>
+                  <Col><ContentCard profileImage={imgList[1]} member={memberList[1]} text={textList[1]} link={gitLink[1]}></ContentCard></Col>
+                  <Col><ContentCard profileImage={imgList[2]} member={memberList[2]} text={textList[2]} link={gitLink[2]}></ContentCard></Col>
+                  <Col><ContentCard profileImage={imgList[3]} member={memberList[3]} text={textList[3]} link={gitLink[3]}></ContentCard></Col>
+                  <Col><ContentCard profileImage={imgList[4]} member={memberList[4]} text={textList[4]} link={gitLink[4]}></ContentCard></Col>
+                  
+                </Row>
               </div>
 
 
