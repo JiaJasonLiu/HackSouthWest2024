9e9f85a7062d981befc2e0d76a155ff37bc150f1
JiaJasonLiu <jialiu505@gmail.com>
2024-02-18 09:26:00 +0000

    updating get stock data

--- src/server_funcs.py
+++ src/server_funcs.py
@@ -12,24 +12,29 @@ def get_stock_info():
     """Returns stock info dict to be sent to server"""
 
     url = "https://yh-finance.p.rapidapi.com/market/v2/get-quotes"
-
-    querystring = {"region":"US","symbols":"AMD,IBM,AAPL,BTC-USD,MSFT"}
+    companies = ["AMD","IBM","AAPL","BTC-USD","MSFT"]
+    querystring = {"region":"US","symbols":",".join(companies)}
 
     headers = {
         "X-RapidAPI-Key": get_api_key.get(),
         "X-RapidAPI-Host": "yh-finance.p.rapidapi.com"
     }
-
     response = requests.get(url, headers=headers, params=querystring)
-
+    if response.status_code != 200:
+        print("getting stock data error status: ", response.status_code)
     data = json.loads(response.text)
+    if data['quoteResponse']['error'] is not None:
+        print("getting stock data error ", data['quoteResponse']['error'])
+        
     stockData = dict()
-    for company in data['quoteResponse']['result']:
+    for index, company in enumerate(data['quoteResponse']['result']):
         companyData = [str(company['regularMarketPrice']), str(company['regularMarketChange']), 
                        str(company['regularMarketChangePercent'])]
         # price;change;percentageChange
-        stockData[company['longName']] = ";".join(companyData)
+        key = company['longName'] + ";" + companies[index]
+        stockData[key] = ";".join(companyData)
         
+    print(stockData)
     return stockData
 
 def recive_data_from_client(databaseName, data, username):
