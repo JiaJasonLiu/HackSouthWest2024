b22ed947655d166262f917663c00bdb05c11686a
JiaJasonLiu <jialiu505@gmail.com>
2024-02-18 10:25:22 +0000

    dynamic stocks

--- frontend/src/components/stockRotator.jsx
+++ frontend/src/components/stockRotator.jsx
@@ -3,7 +3,7 @@ import 'react-alice-carousel/lib/alice-carousel.css';
 import axios from 'axios';
 import React from 'react';
 
-const baseURL = "/get_stock_price"
+const baseURL = "http://localhost:3001/get_stock_price"
 
 function StockRotator() {
 
@@ -14,49 +14,42 @@ function StockRotator() {
         568: { items: 2 },
         1024: { items: 5 },
     };
-
-    var stocks = [
-        ["BTC", "£50000"],
-        ["ETH", "£70000"],
-        ["AAPL", "£50000"],
-        ["MSFT", "£10000"],
-        ["Bitcoin", "£50000"],
-    ]
-
-    // React.useEffect(() => {
-    //     axios.get(baseURL).then((response) => {
-    //       setPost(response.data);
-    //     });
-    //   }, []);
     
-    //   if (!post) return null;
+    var stocks = []
 
-    var stocks2 = post;
+    React.useEffect(() => {
+        axios.get(baseURL).then((response) => {
+          setPost(response.data);
+        });
+      }, []);
 
+    if (!post) return null;
+    for (const [key, value] of Object.entries(post)) {
+        stocks.push([key.split(";")[1], "$" + value.split(";")[0]])
+    }
     const items = [
-        <a className="item" data-value="1" href="/stocks/?stock={stock[0][0]}">
+        <a className="item" data-value="1" href={"/stocks?=" + stocks[0][0]}>
         {/* // <a className="item" data-value="1" href="/stocks" onclick="location.href=this.href+'?stock='+stocks[0][0]"> */}
             {/* <img className = 'pfpimage' src='../images/logo192.png' alt='imgHERE'></img> */}
             <h3>{stocks[0][0]}</h3>
             <h4>{stocks[0][1]}</h4>
-            <h5>{stocks2}</h5>
         </a>,
-        <a className="item" data-value="1" href="/stocks">
+        <a className="item" data-value="1" href={"/stocks?=" + stocks[1][0]}>
             {/* <img className = 'pfpimage' src='../images/logo192.png' alt='imgHERE'></img> */}
             <h3>{stocks[1][0]}</h3>
             <h4>{stocks[1][1]}</h4>
         </a>,
-            <a className="item" data-value="1" href="/stocks">
+            <a className="item" data-value="1" href={"/stocks?=" + stocks[2][0]}>
             {/* <img className = 'pfpimage' src='../images/logo192.png' alt='imgHERE'></img> */}
             <h3>{stocks[2][0]}</h3>
             <h4>{stocks[2][1]}</h4>
         </a>,
-            <a className="item" data-value="1" href="/stocks">
+            <a className="item" data-value="1" href={"/stocks?=" + stocks[3][0]}>
             {/* <img className = 'pfpimage' src='../images/logo192.png' alt='imgHERE'></img> */}
             <h3>{stocks[3][0]}</h3>
             <h4>{stocks[3][1]}</h4>
         </a>,
-            <a className="item" data-value="1" href="/stocks">
+            <a className="item" data-value="1" href={"/stocks?=" + stocks[4][0]}>
             {/* <img className = 'pfpimage' src='../images/logo192.png' alt='imgHERE'></img> */}
             <h3>{stocks[4][0]}</h3>
             <h4>{stocks[4][1]}</h4>
--- frontend/src/pages/Stock.jsx
+++ frontend/src/pages/Stock.jsx
@@ -4,32 +4,65 @@ import StockRotator from '../components/stockRotator';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
 import React from 'react';
-import Popup from 'reactjs-popup';
-import 'reactjs-popup/dist/index.css';
+// import 'reactjs-popup/dist/index.css';
+import axios from 'axios';
+
+
+const baseURL = "http://localhost:3001/get_stock_price"
 
 function Stock() {
+  
+  const [post, setPost] = React.useState(null);
+  var stock = window.location.href.split("=")[1]
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  var stockName
+  var stockPrice
+  var stockChange
+  var stockChangePercent
+
+
+  if (!post) return null;
+  for (const [key, value] of Object.entries(post)) {
+    if (key.split(";")[1] === stock){
+      stockName = key.split(";")[0]
+      stockPrice = value.split(";")[0]
+      stockChange = value.split(";")[1]
+      if (stockChange > 0){
+        stockChange = "+" + stockChange
+      }
+
+      stockChangePercent = value.split(";")[2]
+    }
+  }
+  const stockPriceDiscounted = (stockPrice * .80).toFixed(2)
+
   return (
     <div className="stock">
       <div className='title1 container'>
       <StockRotator></StockRotator>
       </div>
       <div className='container'>
-        <h1 className='centered title1'>BUY YOUR ETHERIUM!!!</h1>
+        <h1 className='centered title1'>BUY YOUR {stock}!!!</h1>
       </div>
       <div className='stockInfo'>
         <Container className='stockInfo'>
             <Row>
                 <Col>
                     <div>
-                        <p className='stockName'>Etherium</p>
-                        <p className='stockInfo'>ETH</p>
-                        <p className='stockInfo'>£50000</p>
-                        <p className='stockInfo'>(0.0014%)</p>
-                        <Button className='butt' variant="danger">BUY</Button>
-                        <Button className='butt' variant="danger">SELL</Button>
-
+                        <b className='stockName'>{stockName}</b>
+                        <p className='stockInfo'>
+                          <b>Market Price: </b> {stockPrice}</p>
+                        <p className='stockInfo'>
+                          <b>Asia++ Price: </b> {stockPriceDiscounted} (-20%)</p>
+                        <p className='stockInfo'>{stockChange}</p>
+                        <p className='stockInfo'>({stockChangePercent}%)</p>
                     </div>
                 </Col>
                 <Col>
